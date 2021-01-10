83ca5a9bfc69b1e4d56d9d8e8f7224a3fa53b376
lin chuan <linch1982@gmail.com>
2021-01-10 16:29:56 +0800

    增加日志说明

--- blog/src/main/java/com/example/blog/config/SecurityConfig.java
+++ blog/src/main/java/com/example/blog/config/SecurityConfig.java
@@ -35,9 +35,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     // 去使能csrf校验
     http.csrf().disable();
+    // 去使能httpBasic认证
+    http.httpBasic().disable();
+
     // 配置从哪个url的请求中获取用户名, 密码并进行认证
     http.formLogin().loginProcessingUrl("/login");
-    http.httpBasic().disable();
+
+    // 配置访问权限
+    http.authorizeRequests().antMatchers("/blog/**").hasRole("ADMIN");
   }
 
   @Override
