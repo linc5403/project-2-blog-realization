2c6de18a272c6da43123203e56a1b150c6396b1d
lin chuan <linch1982@gmail.com>
2021-03-01 11:48:27 +0800

    update security

--- blog/src/main/java/com/example/blog/BlogApplication.java
+++ blog/src/main/java/com/example/blog/BlogApplication.java
@@ -1,16 +1,14 @@
 package com.example.blog;
 
-import com.example.blog.bean.Blog;
-import com.example.blog.bean.Comment;
-import com.example.blog.bean.User;
+import com.example.blog.dao.BlogDao;
+import com.example.blog.dao.UserDao;
 import com.example.blog.utils.MapperUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Date;
 
 @SpringBootApplication
 @MapperScan
@@ -19,7 +17,7 @@ public class BlogApplication {
 
   public static void main(String[] args) throws IllegalAccessException {
     var app = SpringApplication.run(BlogApplication.class, args);
-    /*var userDao = app.getBean(UserDao.class);
+    var userDao = app.getBean(UserDao.class);
     var user = userDao.getUserByName("王二");
     log.debug(user.toString());
     var user2 = userDao.getUserById(2);
@@ -35,9 +33,9 @@ public class BlogApplication {
     System.out.println(mapper);
     blog1 = blogDao.getBlogById(220);
     mapper = MapperUtil.removeNullFields(blog1);
-    System.out.println(mapper);*/
+    System.out.println(mapper);
 
-    Blog blog = new Blog();
+    /*Blog blog = new Blog();
     blog.setTitle("abc");
     User blogger = new User();
     blogger.setUsername("张三");
@@ -46,10 +44,10 @@ public class BlogApplication {
     comment.setContent("这是一个评论");
     comment.setBlog(blog);
     blog.setAuthor(blogger);
-    blog.setComments(new ArrayList<>(Collections.singletonList(comment)));
+    blog.setComments(new ArrayList<>(Collections.singletonList(comment)));*/
     //    var s = JSON.toJSON(blog);
     //    System.out.println(s);
-    var out = MapperUtil.removeNullFields(blog);
-    System.out.println(out);
+    /*var out = MapperUtil.removeNullFields(blog);
+    System.out.println(out);*/
   }
 }
--- blog/src/main/java/com/example/blog/config/SecurityConfig.java
+++ blog/src/main/java/com/example/blog/config/SecurityConfig.java
@@ -1,77 +1,86 @@
 package com.example.blog.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import com.example.blog.bean.User;
+import com.example.blog.service.UserService;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 @EnableWebSecurity
-@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  private final UserDetailsService userDetailsService;
+  private final UserService userService;
 
-  public SecurityConfig(UserDetailsService userDetailsService) {
-    this.userDetailsService = userDetailsService;
-  }
-
-  /**
-   * 父类的缺省配置是:
-   *
-   * <pre>http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
-   * </pre>
-   *
-   * 主要是配置public的endpoint, 以及对应url的权限
-   *
-   * @param http - security config
-   * @throws Exception - other exceptions
-   */
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    // 去使能csrf校验
-    http.csrf().disable();
-    // 去使能httpBasic认证
-    http.httpBasic().disable();
-
-    http.formLogin()
-        // 配置从哪个url的请求中获取用户名, 密码并进行认证
-        .loginProcessingUrl("/login")
-        .successHandler(successHandler())
-        .failureHandler(failureHandler());
-
-    // 配置访问权限
-    http.authorizeRequests().anyRequest().permitAll();
-    //    http.authorizeRequests().antMatchers("/blog/**").hasRole("User");
-  }
-
-  private AuthenticationSuccessHandler successHandler() {
-    return (httpServletRequest, httpServletResponse, authentication) -> {
-      httpServletResponse.getWriter().append("OK");
-      httpServletResponse.setStatus(200);
-    };
-  }
-
-  private AuthenticationFailureHandler failureHandler() {
-    return (httpServletRequest, httpServletResponse, e) -> {
-      httpServletResponse.getWriter().append("Authentication failure");
-      httpServletResponse.setStatus(401);
-    };
+  public SecurityConfig(UserService userService) {
+    this.userService = userService;
   }
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userDetailsService);
+    auth.inMemoryAuthentication().withUser("aaa").password("{noop}aaa").roles("USER");
+    auth.userDetailsService(new UserDetailsService() {
+      @Override
+      public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.getUserByName(username);
+        // 根据自定义的user对象生成UserDetails对象
+        return new UserDetailsImp(user);
+      }
+    }).passwordEncoder(NoOpPasswordEncoder.getInstance());
+  }
+}
+
+class UserDetailsImp implements UserDetails {
+
+  private final User user;
+
+  UserDetailsImp(User user) {
+    this.user = user;
   }
 
-  @Bean
-  PasswordEncoder encoder() {
-    return NoOpPasswordEncoder.getInstance();
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    var roles = new ArrayList<GrantedAuthority>();
+    for (var role : user.getRoles()) {
+      roles.add((GrantedAuthority) () -> "ROLE_" + role);
+    }
+    return roles;
+  }
+
+  @Override
+  public String getPassword() {
+    return user.getPassword();
+  }
+
+  @Override
+  public String getUsername() {
+    return user.getUsername();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return user.getActivated();
   }
 }
